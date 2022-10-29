0ab2364cc4fa6bcd28c184b475ae430ef07fa95d
premgoud <67771169+premgoud@users.noreply.github.com>
2022-10-29 12:12:17 +0530

    get api for registration status is  done adding routes for get api

---
+++ Model/GetRegistartionstatus.php
@@ -0,0 +1,10 @@
+<?php
+require_once PROJECT_ROOT_PATH . "/Model/Database.php";
+ 
+class GetRegistartionstatus extends Database
+{
+    public function getUsers($email)
+    {
+        return $this->select("SELECT id FROM air_passenger_profile where email=$email",[] );
+    }
+}
