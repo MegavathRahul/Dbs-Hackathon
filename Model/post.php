6882ad0f63b8e9518c50d403b797fd291c65f0c9
AmriteshSingh <71891409+AmriteshSingh-c@users.noreply.github.com>
2022-10-29 13:38:44 +0530

    Update post.php

--- Model/post.php
+++ Model/post.php
@@ -1,6 +1,14 @@
 <?php
 
 include 'Database.php";
+require_once "/inc/config.php";
+
+$link = mysqli_connect($DBhostname, $DBusername, $DBpassword, $DBname);
+
+if (mysqli_connect_errno()) {
+   die("Connect failed: %s\n" + mysqli_connect_error());
+   exit();
+}
 
 $method=$_SERVER['REQUEST_METHOD'];
 $request=explode('/',trim($_SERVER['PATH_INFO'],'/'));
